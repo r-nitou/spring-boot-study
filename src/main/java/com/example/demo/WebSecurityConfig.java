b6b643b118d1513f1bd50d854998f7e1f378cc1d
DESKTOP-43NISUT\R_Nitou <r_nitou@dworks-ent.co.jp>
2026-04-08 12:12:01 +0900

    ユーザー認証機能の追加

---
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration		//これが設定クラスであることを示す
+@EnableWebSecurity	
+public class WebSecurityConfig {
+	@Bean			//認証のルールを決める設定
+	public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+		http
+		.authorizeHttpRequests((requests)->requests
+				.anyRequest().authenticated()					//すべてのリクエストに認証が必要
+				)
+				.formLogin((from)->from.permitAll()				//ログイン画面は誰でも見れる
+				);
+		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean			//ユーザー情報をメモリ上に作成する
+	public UserDetailsService userDetailsService() {
+		UserDetails user =User.builder()
+					.username("admin")					//ユーザー名
+					.password(passwordEncoder().encode("password"))				//パスワード
+					.roles("USER")
+					.build();
+		
+		return new InMemoryUserDetailsManager(user);
+	}
+}
+
+
